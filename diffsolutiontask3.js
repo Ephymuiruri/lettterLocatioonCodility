161b3d207a9fe117286cbdbf793d18f8e5954426
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-05-27 11:23:42 +0300

    added extra solutions

---
+++ diffsolutiontask3.js
@@ -0,0 +1,11 @@
+function digitadd(arr){
+    for(no of arr){
+       const noArray = no.toString().split('')
+       const sumArray =[]
+       console.log(noArray)
+       for(let i=0;i < noArray.length;i++){
+           sumArray.push(parseInt(noArray[i]))
+       }
+    }
+   }
+   digitadd([1, 10, 12, 3])
